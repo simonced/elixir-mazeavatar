67cae1482b56f7185c44dcadab457cdb2f0de9e6
cedric simon <simonced@gmail.com>
2017-05-21 00:28:36 +0900

    add digging holes in the walls at a specific position
    coordinates logic added (should be enough)

--- lib/mazeAvatar.ex
+++ lib/mazeAvatar.ex
@@ -26,15 +26,39 @@ defmodule MazeAvatar do
 
   # generate a grid of the maze size
   # with only walls!
+  # @return %{width, height, cells (list)}
   # each cell is a map %{x (int), y (int), wall (boolean)}
   def fillGrid(width_, height_) do
     # making a grid where each entry is a map with position and a flag
-    for x <- 0..width_-1, y <- 0..height_-1, do: %{x: x, y: y, wall: true}
+    %{
+      width: width_,
+      height: height_,
+      cells: (for x <- 0..width_-1, y <- 0..height_-1, do: %{x: x, y: y, wall: true})
+    }
+  end
+
+
+  # calculate position in grid
+  # @param grid a grid generated with fillGrid
+  # @param x_ and y_ int
+  # @return int
+  def getPos(maze_, x_, y_) do
+    maze_.width * x_ + y_
+  end
+
+
+  # return a cell from the maze at coordinates x y
+  def getCellAt(maze_, x_, y_) do
+    pos = getPos(maze_, x_, y_)
+    Enum.at(maze_.cells, pos)
   end
 
 
   # returns a new grid with a cell dug at position x y
-  def digCellAt(grid_, x_, y_) do
-    IO.puts "=== TODO ==="
+  def digCellAt(maze_, x_, y_) do
+    %{width: maze_.width,
+      height: maze_.height,
+      cells: List.replace_at(maze_.cells, getPos(maze_, x_, y_), %{x: x_, y: y_, wall: false})
+    }
   end
 end
--- test/mazeAvatar_test.exs
+++ test/mazeAvatar_test.exs
@@ -4,26 +4,36 @@ defmodule MazeAvatarTest do
 
 
   test "init grid" do
-    assert MazeAvatar.fillGrid(2, 2) == [
+    assert MazeAvatar.fillGrid(2, 2) == %{width: 2, height: 2, cells: [
       %{x: 0, y: 0, wall: true},
       %{x: 0, y: 1, wall: true},
+      %{x: 1, y: 0, wall: true},
+      %{x: 1, y: 1, wall: true}
+    ]}
+  end
+
+
+  test "get position" do
+    maze = MazeAvatar.fillGrid(2, 2)
+    assert MazeAvatar.getPos(maze, 1, 1) == 3
+  end
+
+
+  test "dig a hole" do
+    maze = MazeAvatar.fillGrid(2,2)
+    maze2 = MazeAvatar.digCellAt(maze, 0, 1)
+
+    assert maze2.cells == [
+      %{x: 0, y: 0, wall: true},
+      %{x: 0, y: 1, wall: false},
       %{x: 1, y: 0, wall: true},
       %{x: 1, y: 1, wall: true}
     ]
   end
 
 
-  test "find position" do
-    grid = MazeAvatar.fillGrid(2,2)
-    grid2 = MazeAvatar.digCellAt(grid, 0, 1)
-
-    assert grid2 == [
-      %{x: 0, y: 0, wall: false},
-      %{x: 0, y: 1, wall: true},
-      %{x: 1, y: 0, wall: false},
-      %{x: 1, y: 1, wall: false}
-    ]
+  test "draw Ascii maze" do
+    IO.puts "TODO"
   end
-
 
 end
